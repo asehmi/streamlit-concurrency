78c3a28cbbe1ca97cd2825653251d1d8f9330a11
Wang Guan <momocraft@gmail.com>
2025-04-23 23:50:45 +0900

    update log

--- pages/1_troubled_multithreading.py
+++ pages/1_troubled_multithreading.py
@@ -1,14 +1,13 @@
 import streamlit as st
+import logging
 import threading
 import time
 import streamlit_concurrency.demo as demo
 
 st.markdown("""
-This page demostrates incorrect cases of multithreading in streamlit.
+This page demostrates incorrect multithreading in streamlit.
             
----
-
-Please click a button and check exceptions and warnings in the console.
+Please click a button and observe this page and the console.
 """)
 
 st.session_state["foo"] = "foo-value"
@@ -32,7 +31,7 @@ def get_data():
 class CustomThreadUpdatingWidget(threading.Thread):
     def run(self):
         # sleeping
-        time.sleep(2)
+        time.sleep(1)
         # a custom thread can call @st.cache_data() function
         data = get_data()
         assert data == "dummy"
@@ -45,15 +44,19 @@ class CustomThreadReadingSessionState(threading.Thread):
     def run(self):
         value = st.session_state.get("foo", None)
         assert value is None, "Session state should be None in a new thread"
-        time.sleep(2)
+        time.sleep(1)
         print(
             f"st.session_state['foo'] as seen by {threading.current_thread().name} is {value}"
         )
 
 
-if update_widget_clicked:
-    t = CustomThreadUpdatingWidget()
-    t.start()
-elif read_session_state_clicked:
-    t = CustomThreadReadingSessionState()
-    t.start()
+# NOTE this does not capture errors
+with demo.create_log_record_sink(level=logging.DEBUG) as (records, lines):
+    if update_widget_clicked:
+        t = CustomThreadUpdatingWidget()
+        t.start()
+        t.join()
+    elif read_session_state_clicked:
+        t = CustomThreadReadingSessionState()
+        t.start()
+        t.join()
